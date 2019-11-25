2a573249990e3de182b5f58292bc717815e21525
gemmygeorge13 <gemmygeorge13@users.noreply.github.com>
2019-11-24 23:52:18 -0600

    X

--- src/app/pages/queue/queue.page.ts
+++ src/app/pages/queue/queue.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { timingSafeEqual } from 'crypto';
+//import { timingSafeEqual } from 'crypto';
 
 
 @Component({
