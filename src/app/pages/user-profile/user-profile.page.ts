48b24d41781c45b739af9c25bf7e20950b62aa50
gemmygeorge13 <gemmygeorge13@users.noreply.github.com>
2019-12-03 22:45:23 -0600

    User profile completed

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { MusicService } from 'src/app/services/music.service';
 import { UserInfo } from 'src/app/models/UserInfo';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,19 +10,29 @@ import { UserInfo } from 'src/app/models/UserInfo';
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
-
+  
   userInfo: UserInfo;
   errorMessage: string;
-  userId: number = 3;
-  constructor(private navCtrl: NavController, private musicService: MusicService) { }
+  //userId: number = 3;
+  constructor(private storage: NativeStorage, private navCtrl: NavController, private musicService: MusicService) { }
 
   ngOnInit() {
 
-    this.musicService.getUserInfo(this.userId).subscribe({
-       next: userInfo => this.userInfo = userInfo,
-       error: err => this.errorMessage = err
-    });
+    this.storage.getItem('userId')
+    .then(
+      (val) => { this.setUserInfo(val); },
+      e => console.log(e)
+    );
 
+    
+
+  }
+
+  setUserInfo(userId: number) {
+    this.musicService.getUserInfo(userId).subscribe({
+      next: userInfo => this.userInfo = userInfo,
+      error: err => this.errorMessage = err
+   });
   }
 
 }
